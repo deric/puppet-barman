e735262d23e80a986a9113596af7a97f0fe691bd
Florian Heinle <github@florianheinle.de>
2018-12-27 10:06:53 +0100

    db config files owned by user too

--- manifests/server.pp
+++ manifests/server.pp
@@ -362,7 +362,7 @@ define barman::server (
   file { "/etc/barman.conf.d/${name}.conf":
     ensure  => $ensure,
     mode    => '0640',
-    owner   => 'root',
+    owner   => $user,
     group   => $::barman::settings::group,
     content => template($conf_template),
   }
