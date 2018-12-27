cf90288038f85b62e571d1f76b1aac9b82088d45
Florian Heinle <github@florianheinle.de>
2018-12-27 09:55:42 +0100

    make barman conf dir owned by barman user

--- manifests/init.pp
+++ manifests/init.pp
@@ -419,7 +419,7 @@ class barman (
     ensure  => $ensure_directory,
     purge   => $purge_unknown_conf,
     recurse => true,
-    owner   => 'root',
+    owner   => $user,
     group   => $group,
     mode    => '0750',
     require => Package['barman'],
@@ -427,7 +427,7 @@ class barman (
 
   file { '/etc/barman.conf':
     ensure  => $ensure_file,
-    owner   => 'root',
+    owner   => $user,
     group   => $group,
     mode    => '0640',
     content => template($conf_template),
