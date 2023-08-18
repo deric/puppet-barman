e8d6e0f28454d8a68eb98d09205e3d6502a37836
Tomas Barton <barton.tomas@gmail.com>
2023-08-18 12:54:26 +0200

    Fix rubocop offense

--- spec/classes/barman_spec.rb
+++ spec/classes/barman_spec.rb
@@ -5,7 +5,6 @@ require 'spec_helper'
 describe 'barman' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
-
       let(:facts) { os_facts }
 
       it { is_expected.to compile.with_all_deps }
